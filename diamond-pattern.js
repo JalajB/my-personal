d50c8adeef49cf375b1863c68f1ef22f1de998a9
JalajB <bankar.jalaj@gmail.com>
2026-01-04 16:23:41 +0530

    Trying...

--- diamond-pattern.js
+++ diamond-pattern.js
@@ -1,31 +1,31 @@
-let endCounter = 11;
+let endCounter = 21;
 endString = '';
-for (let i = 0 ; i<= endCounter; i ++) {
-	for (let j = 0 ; j < endCounter  ; j++) {
-		if ( i < (Math.ceil(endCounter/2)) ) {
-			if (i == j ) {
-				endString+= "*";
-			} else {
-				endString += "\t";
-			}
-		} else {
-            
-        }
-	}
-    for (let k = 0 ; k < endCounter; k ++) {
-        if ( i > Math.ceil(endCounter / 2)) {
-            if ( (endCounter - i) == k) {
+for (let i = 0; i <= endCounter; i++) {
+    for (let j = 0; j < endCounter; j++) {
+        if (i < (Math.ceil(endCounter / 2))) {
+            if (i == j) {
                 endString += "*";
             } else {
                 endString += "\t";
             }
-        }        
+        } else {
+
+        }
+    }
+    for (let k = 0; k < endCounter; k++) {
+        if (i > Math.ceil(endCounter / 2)) {
+            if ((endCounter - i) == k) {
+                endString += "*";
+            } else {
+                endString += "\t";
+            }
+        }
     }
     if (i != Math.ceil(endCounter / 2)) {
-        endString += "\n"  ;
+        endString += "\n";
     }
 
 
 
 }
-console.log (endString);
+console.log(endString);
