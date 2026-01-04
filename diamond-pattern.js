1db2cee62b2ac6f85d230b88aab293bb56e6874f
JalajB <bankar.jalaj@gmail.com>
2026-01-04 23:53:41 +0530

    diamond pattern is back again

---
+++ diamond-pattern.js
@@ -0,0 +1,27 @@
+let endCounter = 21;
+endString = "";
+for (let i = 0; i <= endCounter; i++) {
+  for (let j = 0; j < endCounter; j++) {
+    if (i < Math.ceil(endCounter / 2)) {
+      if (i == j) {
+        endString += "*";
+      } else {
+        endString += "\t";
+      }
+    } else {
+    }
+  }
+  for (let k = 0; k < endCounter; k++) {
+    if (i > Math.ceil(endCounter / 2)) {
+      if (endCounter - i == k) {
+        endString += "*";
+      } else {
+        endString += "\t";
+      }
+    }
+  }
+  if (i != Math.ceil(endCounter / 2)) {
+    endString += "\n";
+  }
+}
+console.log(endString);
